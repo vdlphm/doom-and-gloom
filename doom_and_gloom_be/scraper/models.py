75b221bb6b12496c8cb4b11f4cf1ff62a1140883
Keith Pham <minhnghia2208@gmail.com>
2022-11-12 20:18:11 -0500

    update scraper Model and View

--- doom_and_gloom_be/scraper/models.py
+++ doom_and_gloom_be/scraper/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Todo(models.Model):
+class Company(models.Model):
     company_name = models.CharField(max_length = 180)
-    time_update = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
+    time_update = models.DateTimeField()
     layoff_count = models.IntegerField()
 
     def __str__(self):
--- doom_and_gloom_be/scraper/views.py
+++ doom_and_gloom_be/scraper/views.py
@@ -1,36 +1,32 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import permissions
-from .models import Todo
-from .serializers import TodoSerializer
+from .models import Company
+from .serializer import CompanySerializer
 
-class TodoListApiView(APIView):
-    # add permission to check if user is authenticated
-    permission_classes = [permissions.IsAuthenticated]
+class CompanyView(APIView):
 
-    # 1. List all
-    def get(self, request, *args, **kwargs):
-        '''
-        List all the todo items for given requested user
-        '''
-        todos = Todo.objects.filter(user = request.user.id)
-        serializer = TodoSerializer(todos, many=True)
+    def get_all(self, request):
+        serializer = CompanySerializer(Company.objects, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     # 2. Create
-    def post(self, request, *args, **kwargs):
-        '''
-        Create the Todo with given todo data
-        '''
+    def post(self, request):
+        company = Company.objects.get(company_name = request.data.get('company_name'))
+        layoff_count = request.data.get('layoff_count')
+
+        if company:
+            layoff_count += company.layoff_count 
+        
         data = {
-            'task': request.data.get('task'), 
-            'completed': request.data.get('completed'), 
-            'user': request.user.id
+            'company_name': request.data.get('company_name'), 
+            'layoff_count': layoff_count, 
+            'id': request.user.id
         }
-        serializer = TodoSerializer(data=data)
+        serializer = CompanySerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
